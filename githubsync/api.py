62fc04bdef73e699a4c48dd518514a505c299ec4
Mitar <mitar.git@tnode.com>
2012-05-04 14:38:22 +0200

    Initial version.

---
+++ githubsync/api.py
@@ -0,0 +1,69 @@
+import json
+
+from trac.core import *
+from trac.web import IRequestHandler, IRequestFilter, RequestDone, HTTPNotFound
+from trac.versioncontrol import RepositoryManager
+
+class GitHubSync(Component):
+    """This component syncs GitHub repository with local repository used by Trac."""
+    
+    implements(IRequestHandler, IRequestFilter)
+
+    # IRequestFilter methods
+    def pre_process_request(self, req, handler):
+        """Called after initial handler selection, and can be used to change
+        the selected handler or redirect request."""
+
+        if self.match_request(req):
+            # We disable CSRF protection here and force ourselves as a handler
+            req.form_token = None
+            return self
+        
+        return handler
+
+    def post_process_request(req, template, data, content_type):
+        """Do any post-processing the request might need; typically adding
+        values to the template `data` dictionary, or changing template or
+        mime type."""
+
+        return (template, data, content_type)
+
+    # IRequestHandler methods
+    def match_request(self, req):
+        """Return whether the handler wants to process the given request."""
+
+        return req.method == 'POST' and req.path_info == '/githubsync'
+
+    def process_request(self, req):
+        """Process the request."""
+
+        payload = json.loads(req.args.get('payload'))
+
+        repository_name = payload.get('repository', {}).get('name')
+
+        self.env.log.debug("GitHubSync: Got POST request for repository '%s'", repository_name)
+
+        self._process_repository(repository_name)
+
+        req.send_response(200)
+        req.send_header('Content-Type', 'text/plain')
+        req.send_header('Content-Length', 0)
+        req.end_headers()
+
+        raise RequestDone
+
+    def _process_repository(self, name):
+        if not name:
+            return
+
+        rm = RepositoryManager(self.env)
+        trac_repo = rm.get_repository(name)
+
+        if not trac_repo or not hasattr(trac_repo, 'gitrepo'):
+            return
+
+        self.env.log.debug("GitHubSync: Processing repository at '%s'", trac_repo.gitrepo)
+
+        # Pulling from default source (as configured in repo configuration)
+        output = trac_repo.git.repo.fetch('--all', '--prune', '--tags')
+        self.env.log.debug("GitHubSync: git output: %s", output)
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+VERSION = '0.1'
+PACKAGE = 'githubsync'
+
+setup(
+	name = 'GitHubSyncPlugin',
+	version = VERSION,
+	description = "Sync GitHub repository with local repository used by Trac.",
+	author = 'Mitar',
+	author_email = 'mitar.trac@tnode.com',
+	url = 'http://mitar.tnode.com/',
+	keywords = 'trac plugin',
+	license = "AGPLv3",
+	packages = [PACKAGE],
+	include_package_data = True,
+	install_requires = [],
+	zip_safe = False,
+	entry_points = {
+		'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE),
+	},
+)
