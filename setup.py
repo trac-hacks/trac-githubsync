2811f85776937ac0a3ad78f7e9b5d56a34a8bbe8
Mitar <mitar.git@tnode.com>
2013-06-15 19:06:00 -0700

    Tabs to spaces.

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ VERSION = '0.1.3'
 PACKAGE = 'githubsync'
 
 setup(
-	name = 'GitHubSyncPlugin',
-	version = VERSION,
-	description = "Sync GitHub repository with local repository used by Trac.",
-	author = 'Mitar',
-	author_email = 'mitar.trac@tnode.com',
-	url = 'http://mitar.tnode.com/',
-	keywords = 'trac plugin',
-	license = "AGPLv3",
-	packages = [PACKAGE],
-	include_package_data = True,
-	install_requires = [
-		'iptools>=0.6.1',
-	],
-	zip_safe = False,
-	entry_points = {
-		'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE),
-	},
+    name = 'GitHubSyncPlugin',
+    version = VERSION,
+    description = "Sync GitHub repository with local repository used by Trac.",
+    author = 'Mitar',
+    author_email = 'mitar.trac@tnode.com',
+    url = 'http://mitar.tnode.com/',
+    keywords = 'trac plugin',
+    license = "AGPLv3",
+    packages = [PACKAGE],
+    include_package_data = True,
+    install_requires = [
+        'iptools>=0.6.1',
+    ],
+    zip_safe = False,
+    entry_points = {
+        'trac.plugins': '%s = %s' % (PACKAGE, PACKAGE),
+    },
 )
